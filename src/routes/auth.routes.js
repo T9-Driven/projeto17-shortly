5c2056048e0af56db4403b3dd20edcf08e4ccc9b
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-03 19:39:06 -0300

    feat: add sign in function

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,10 +3,11 @@ import { validateSchema } from "../middlewares/schemaValidator.js";
 import loginSchema from "../schemas/loginSchema.js";
 import userSchema from "../schemas/userSchema.js";
 import { createUser } from "../controllers/users.controller.js";
+import { signIn } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post("/signin", validateSchema(loginSchema), () => { });
+router.post("/signin", validateSchema(loginSchema), signIn);
 router.post("/signup", validateSchema(userSchema), createUser);
 
 export default router;
