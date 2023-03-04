d35d24178fed03a3392c4ce0a7637edc41cf4408
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-03 21:27:10 -0300

    feat: user ranking and me function

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-// import { authValidation } from "../middlewares/authorization.middlewares.js";
+import { authValidation } from "../middlewares/authorization.middleware.js";
+import { getLoggedUser, getRanking } from "../controllers/users.controller.js";
 
 const router = Router();
 
-// router.get("/users/me", authValidation, () => { });
-router.get("/ranking", () => { });
+router.get("/users/me", authValidation, getLoggedUser);
+router.get("/ranking", getRanking);
 
 export default router;
